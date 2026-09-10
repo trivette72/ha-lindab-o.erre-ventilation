525cb23392bab7ba74aab1aad7fc6769726d3610
James Trivette <trivette@atlas.cz>
2026-09-10 09:00:28 +0200

    Update const.py

--- custom_components/lindab_ventilation/const.py
+++ custom_components/lindab_ventilation/const.py
@@ -8,7 +8,7 @@ from typing import Final
 from homeassistant.const import Platform
 
 DOMAIN: Final = "lindab_ventilation"
-INTEGRATION_VERSION: Final = "0.1.6"
+INTEGRATION_VERSION: Final = "0.1.8"
 DEFAULT_BASE_URL: Final = "https://app-oerre.it:4521"
 DEFAULT_SCAN_INTERVAL: Final = timedelta(seconds=60)
 DISCOVERY_INTERVAL: Final = timedelta(hours=6)
